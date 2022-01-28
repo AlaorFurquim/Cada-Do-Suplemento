9d6991c0ae150df75aeece51af69a4e82cb9b574
AlaorFurquim <alaorfurquim34@gmail.com>
2022-01-27 21:24:57 -0400

    Criando o carrosel de imagens

---
+++ Casa-Do-Suplemento/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Casa_Do_Suplemento.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
