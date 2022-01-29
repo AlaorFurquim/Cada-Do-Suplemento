88a961281614e466dfa3b7e899cd26386f0a851b
AlaorFurquim <alaorfurquim34@gmail.com>
2022-01-28 21:05:42 -0400

    Criando as classe models Categoria e Suplemento

---
+++ Casa-Do-Suplemento/Models/Categoria.cs
@@ -0,0 +1,14 @@
+﻿namespace Casa_Do_Suplemento.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+
+        public string CategoriaName { get; set; }
+
+        public string Descricao { get; set; }
+
+        public List<Suplemento> Suplementos { get; set; } //relacionamento de um para muito 
+        
+    }
+}
---
+++ Casa-Do-Suplemento/Models/Suplemento.cs
@@ -0,0 +1,28 @@
+﻿namespace Casa_Do_Suplemento.Models
+{
+    public class Suplemento
+    {
+        public int SuplementoId { get; set; }
+
+        public string Nome { get; set; }
+
+        public string DescricaoCurta { get; set; }
+
+        public string DescricaoDetalhada { get; set; }
+
+        public decimal Preco { get; set; }
+
+        public string ImagemUrl { get; set; }
+
+        public string ImagemThumbnailUrl { get; set; }
+
+        public bool IsSuplementoFavorito { get; set; }
+
+        public bool EmEstoque { get; set; }
+
+        public int CategoriaId { get; set; } // chave estrangeira
+
+        public virtual Categoria Categoria { get; set; } // propriedade de navegacao
+
+    }
+}
