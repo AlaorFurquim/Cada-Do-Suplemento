e6b48a7cd0ca01c04073050eeb43984f664c3a63
AlaorFurquim <alaorfurquim34@gmail.com>
2022-03-02 20:06:55 -0400

    Criando a tela e o controller Contato

---
+++ Casa-Do-Suplemento/Controllers/ContatoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Casa_Do_Suplemento.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
