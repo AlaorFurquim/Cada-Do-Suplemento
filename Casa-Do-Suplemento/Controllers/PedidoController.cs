4c8762c5df02764a43671b01b32f20e5994ff8d5
AlaorFurquim <alaorfurquim34@gmail.com>
2022-04-01 20:56:52 -0400

    Criado a tela e o metodo checkout

---
+++ Casa-Do-Suplemento/Controllers/PedidoController.cs
@@ -0,0 +1,30 @@
+﻿using Casa_Do_Suplemento.Models;
+using Casa_Do_Suplemento.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Casa_Do_Suplemento.Controllers
+{
+    public class PedidoController : Controller
+    {
+        private readonly IPedidoRepository _pedidoRepository;
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public PedidoController(IPedidoRepository pedidoRepository, CarrinhoCompra carrinhoCompra)
+        {
+            _pedidoRepository = pedidoRepository;
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Pedido pedido)
+        {
+            return View();
+        }
+
+    }
+}
