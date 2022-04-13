cdf0d6111234b886c87f2232b014f0012ae91344
AlaorFurquim <alaorfurquim34@gmail.com>
2022-04-13 19:19:28 -0400

    Criando a tela de Registro

--- Casa-Do-Suplemento/Controllers/AccountController.cs
+++ Casa-Do-Suplemento/Controllers/AccountController.cs
@@ -47,5 +47,31 @@ namespace Casa_Do_Suplemento.Controllers
             return View(loginVM);
         }
 
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(LoginViewModel registroVM)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser { UserName = registroVM.UserName };
+                var result = await _userManager.CreateAsync(user, registroVM.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Login", "Account");
+                }
+                else
+                {
+                    this.ModelState.AddModelError("Registro", "Falha ao registrar o usuário");
+                }
+            }
+            return View(registroVM);
+        }
+
     }
 }
