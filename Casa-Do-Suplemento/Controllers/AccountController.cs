0f20c25df633782f908ed4f5db74fe5abe697da6
AlaorFurquim <alaorfurquim34@gmail.com>
2022-04-14 20:00:10 -0400

    Colocando o link de login e register na pagina home

--- Casa-Do-Suplemento/Controllers/AccountController.cs
+++ Casa-Do-Suplemento/Controllers/AccountController.cs
@@ -73,5 +73,14 @@ namespace Casa_Do_Suplemento.Controllers
             return View(registroVM);
         }
 
+        [HttpPost]
+        public async Task <IActionResult> Logout()
+        {
+            HttpContext.Session.Clear();
+            HttpContext.User = null;
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
     }
 }
