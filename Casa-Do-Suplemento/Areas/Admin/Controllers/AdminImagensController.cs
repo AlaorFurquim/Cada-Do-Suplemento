bcca5ebf00eb155bc3acfec2131abb4b90b7ae15
AlaorFurquim <alaorfurquim34@gmail.com>
2022-05-02 21:21:46 -0400

    Criando a tela de gerenciador de imagens

---
+++ Casa-Do-Suplemento/Areas/Admin/Controllers/AdminImagensController.cs
@@ -0,0 +1,28 @@
+﻿using Casa_Do_Suplemento.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace Casa_Do_Suplemento.Areas.Admin.Controllers
+{
+
+    [Area ("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class AdminImagensController : Controller
+    {
+        private readonly ConfigurationImagens _myConfig;
+        private readonly IWebHostEnvironment _HostingEnvironment;
+
+        public AdminImagensController(IWebHostEnvironment hostingEnvironment,
+            IOptions<ConfigurationImagens>myConfiguration)
+        {
+            _myConfig = myConfiguration.Value;
+            _HostingEnvironment = hostingEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
